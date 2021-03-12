48d4fbc18c2b91cad15d9353850278029b735715
diaojiahuitest <diaojiahuitest@qq.com>
2021-03-12 16:55:00 +0800

    编程数学基础与，或，异或

--- binary-demo/src/main/java/com/djh/math/binary/main/MathBinaryMain.java
+++ binary-demo/src/main/java/com/djh/math/binary/main/MathBinaryMain.java
@@ -2,6 +2,8 @@ package com.djh.math.binary.main;
 
 import com.djh.math.binary.utils.LessonUtils;
 
+import static com.djh.math.binary.utils.LessonUtils.decimalToBinary;
+
 /**
  * 数学二元主要
  *
@@ -10,13 +12,13 @@ import com.djh.math.binary.utils.LessonUtils;
  */
 public class MathBinaryMain {
     public static void main(String[] args) {
-        int a = 53;
+        int a = 3890;
         String b = "110101";
 
         /*int a = 106;
         String b = "1101010";*/
         // 获取十进制数53的二进制数
-        System.out.println(String.format("数字%d的二进制是%s", a, LessonUtils.decimalToBinary(a)));
+        System.out.println(String.format("数字%d的二进制是%s", a, decimalToBinary(a)));
         // 获取二进制数110101的十进制数
         System.out.println(String.format("数字%s的十进制是%d", b, LessonUtils.binaryToDecimal(b)));
 
@@ -33,9 +35,21 @@ public class MathBinaryMain {
 
         m = 3;
 
+        int numA = 53;
+        int numB = 35;
         // 测试向左移位
         System.out.println(String.format("数字%d的二进制向左移%d位是%d", num, m, LessonUtils.leftShift(num, m)));
         // 测试向右移位
         System.out.println(String.format("数字%d的二进制向右移%d位是%d", num, m, LessonUtils.rightShift(num, m)));
+
+        System.out.println("==================================================================");
+        //获取十进制数53和35的按位“或”
+        System.out.println(String.format("数字%d(%s)和数字%d(%s)的按位‘或’结果是%d(%s)", numA, decimalToBinary(numA), numB, decimalToBinary(numB), LessonUtils.or(numA, numB), decimalToBinary(LessonUtils.or(numA, numB))));
+
+        //获取十进制数53和35的按位“与”
+        System.out.println(String.format("数字%d(%s)和数字%d(%s)的按位‘与’结果是%d(%s)", numA, decimalToBinary(numA), numB, decimalToBinary(numB), LessonUtils.and(numA, numB), decimalToBinary(LessonUtils.and(numA, numB))));
+
+        //获取十进制数53和35的按位“异或”
+        System.out.println(String.format("数字%d(%s)和数字%d(%s)的按位‘异或’结果是%d(%s)", numA, decimalToBinary(numA), numA, decimalToBinary(numA), LessonUtils.xor(numA, numA), decimalToBinary(LessonUtils.xor(numA, numA))));
     }
 }
--- binary-demo/src/main/java/com/djh/math/binary/utils/LessonUtils.java
+++ binary-demo/src/main/java/com/djh/math/binary/utils/LessonUtils.java
@@ -57,4 +57,37 @@ public class LessonUtils {
     public static int rightShift (int num, int m) {
         return num >>> m;
     }
+
+    /**
+     * 二进制按位”或“的操作
+     *
+     * @param num1 第一个数字
+     * @param num2 第二个数字
+     * @return int 二进制按位”或“的结果
+     */
+    public static int or (int num1, int num2) {
+        return (num1 | num2);
+    }
+
+    /**
+     * 二进制按位“与”的操作
+     *
+     * @param num1 第一个数字
+     * @param num2 第二个数字
+     * @return int 二进制按位”与“的结果
+     */
+    public static int and (int num1, int num2) {
+        return (num1 & num2);
+    }
+
+    /**
+     * 二进制按位“异或”的操作
+     *
+     * @param num1 第一个数字
+     * @param num2 第二个数字
+     * @return int 二进制按位”与“的结果
+     */
+    public static int xor (int num1, int num2) {
+        return (num1 ^ num2);
+    }
 }
